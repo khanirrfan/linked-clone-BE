6f83efb76651a7bb239796c16e09ccf7419cb924
irrfan13 <irfan.khan@enquero.com>
2021-06-22 12:54:18 +0530

    main server created

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello world!")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "", http.StatusBadRequest)
+		}
+		fmt.Printf("Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello goodbye")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
